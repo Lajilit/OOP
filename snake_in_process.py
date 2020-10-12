0a248e5a3eb485f66d0b5f6e2a40ff5fdf125991
Lajilit <lajilit@gmail.com>
2020-10-12 14:17:14 +0300

    change create_rectangle to create_text

--- snake_in_process.py
+++ snake_in_process.py
@@ -7,29 +7,20 @@ seg_size = 10
 
 # Setting up window
 root = Tk()
-root.title("PythonicWay Snake")
+root.title("Python Snake")
 c = Canvas(root, width=width, height=height, bg="#000000")
 c.grid()
 
 # From OOP
-x1 = 100
-y1 = 200
-color1 = 'white'
-c.create_rectangle(x1, y1, x1 + seg_size, y1 + seg_size, fill=color1)
-
-x2 = 200
-y2 = 400
-color2 = 'green'
-c.create_rectangle(x2, y2, x2 + seg_size, y2 + seg_size, fill=color2)
 
 
-def create_rectangle(x, y, color):
-    return c.create_rectangle(x, y, x + seg_size, y + seg_size, fill=color)
+def create_text(x, y, text, color='white'):
+    return c.create_text(x, y, text=text, font='Arial 20', fill=color)
 
 
-create_rectangle(300, 490, 'red')
-
-
+create_text(200, 400, '*')
+create_text(100, 200, '#')
 
 # Start window
 root.mainloop()
+
